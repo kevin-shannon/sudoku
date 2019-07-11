c4ed0d7fbf49d126ea7da112005d49072fac03ea
unknown <kevers429@gmail.com>
2019-07-10 21:51:10 -0700

    added solve

---
+++ js/DLX.js
@@ -0,0 +1,215 @@
+class Node {}
+
+var header = new Node();
+var rows = 729;
+var cols = 324;
+var solution = [];
+var found = false;
+
+var getRight = (i) => (i + 1) % cols;
+var getLeft = (i) => (i - 1 < 0) ? cols - 1 : i - 1;
+var getUp = (i) => (i - 1 < 0) ? rows : i - 1;
+var getDown = (i) => (i + 1) % (rows + 1);
+
+function initBitMatrix() {
+  var bitMatrix = make2d(rows+1);
+  // Building Block matricies
+  var I = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+  var l = [[1], [1], [1]];
+
+  // Building up the constraints using kronecker products
+  rc = kron(kron(kron(I, I), kron(I, I)), kron(l, l));
+  rn = kron(kron(kron(I, I), kron(l, l)), kron(I, I));
+  cn = kron(kron(l, l), kron(kron(I, I), kron(I, I)));
+  bn = kron(kron(kron(kron(I, l), I), l), kron(I, I));
+
+  // Add row of 0's at top :/
+  bitMatrix[0] = new Array(cols).fill(0);
+
+  // Combine the four arrays side by side
+  for (var i = 0; i < rows; i++)
+    bitMatrix[i+1] = rc[i].concat(rn[i].concat(cn[i].concat(bn[i])));
+  return bitMatrix
+}
+
+function initNodeMatrix() {
+  var nodeMatrix = make2d(rows+1);
+  for (var i = 0; i <= rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      nodeMatrix[i][j] = new Node();
+    }
+  }
+  return nodeMatrix;
+}
+
+// Builds circular doubly linked list
+function makeDonut(bitMatrix, nodeMatrix) {
+  var a, b;
+  // Extra row for headers
+  for (var i = 0; i <= rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      if(!i)
+        nodeMatrix[0][j].nodeCnt = 0;
+
+      // 1 corresponds with node
+      if (bitMatrix[i][j] || !i) {
+        // Increment nodeCnt if not a header node
+        if (i)
+          nodeMatrix[0][j].nodeCnt++;
+
+        nodeMatrix[i][j].column = nodeMatrix[0][j];
+        nodeMatrix[i][j].rowID = i;
+        nodeMatrix[i][j].colID = j;
+
+        // Link nodes
+
+        a = i; b = j;
+        do {b = getLeft(b);} while(!bitMatrix[a][b] && i && b != j);
+        nodeMatrix[i][j].left = nodeMatrix[i][b];
+
+        a = i; b = j;
+        do {b = getRight(b);} while(!bitMatrix[a][b] && i && b != j);
+        nodeMatrix[i][j].right = nodeMatrix[i][b];
+
+        a = i; b = j;
+        do {a = getUp(a);} while(!bitMatrix[a][b] && a && a != i);
+        nodeMatrix[i][j].up = nodeMatrix[a][j];
+
+        a = i; b = j;
+        do {a = getDown(a);} while(!bitMatrix[a][b] && a && a != i);
+        nodeMatrix[i][j].down = nodeMatrix[a][j];
+      }
+      else {
+        nodeMatrix[i][j] = null;
+      }
+    }
+  }
+  // Link header
+  header.right = nodeMatrix[0][0];
+  header.left = nodeMatrix[0][cols - 1];
+
+  nodeMatrix[0][0].left = header;
+  nodeMatrix[0][cols - 1].right = header;
+}
+
+function cover(target) {
+  var colNode = target.column;
+
+  // Unlink target
+  colNode.left.right = colNode.right;
+  colNode.right.left = colNode.left;
+
+  // Move down and remove nodes traversing to the right
+  for (var rowNode = colNode.down; rowNode.rowID != colNode.rowID; rowNode = rowNode.down) {
+    for (var rightNode = rowNode.right; rightNode.colID != rowNode.colID; rightNode = rightNode.right) {
+      rightNode.up.down = rightNode.down;
+      rightNode.down.up = rightNode.up;
+      nodeMatrix[0][rightNode.colID].nodeCnt--;
+    }
+  }
+}
+
+function uncover(target) {
+  var colNode = target.column;
+
+  // Move down and remove nodes traversing to the right
+  for (var rowNode = colNode.up; rowNode.rowID != colNode.rowID; rowNode = rowNode.up) {
+    for (var leftNode = rowNode.left; leftNode.colID != rowNode.colID; leftNode = leftNode.left) {
+      leftNode.up.down = leftNode;
+      leftNode.down.up = leftNode;
+      nodeMatrix[0][leftNode.colID].nodeCnt++;
+    }
+  }
+
+  // Relink target
+  colNode.left.right = colNode;
+  colNode.right.left = colNode;
+}
+
+function getMinColumn() {
+  var h = header;
+  var minCol = h.right;
+  h = h.right.right;
+  do {
+    if (h.nodeCnt < minCol.nodeCnt)
+      minCol = h;
+    h = h.right;
+  } while(h != header);
+  return minCol;
+}
+
+// Returns Node in row, if any
+function getNodeFromRow(row) {
+  for (var i = 0; i < cols; i++) {
+    if (nodeMatrix[row][i] && nodeMatrix[row][i].column.left.right == nodeMatrix[row][i].column)
+      return nodeMatrix[row][i];
+  }
+}
+
+// Covers all columns that have associated node in row
+function choose(row) {
+  var rowNode = getNodeFromRow(row);
+  cover(rowNode);
+  for (var rightNode = rowNode.right; rightNode.colID != rowNode.colID; rightNode = rightNode.right)
+    cover(rightNode);
+}
+
+// Chose all rows in grid
+function chooseGiven(grid) {
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      if (grid[i][j]) {
+        choose(81*i + 9*j + parseInt(grid[i][j]));
+        solution.push(81*i + 9*j + parseInt(grid[i][j]));
+      }
+    }
+  }
+}
+
+// Convert rowNodes to a grid
+function convertSolution(solution) {
+  var grid = make2d(9);
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      grid[i][j] = solution[9*i + j] - (81*i) - (9*j);
+    }
+  }
+  return grid;
+}
+
+function outputSolution(solution) {
+  solution.sort((a, b) => a - b);
+  var finalSolution = convertSolution(solution);
+  updateTable(finalSolution);
+}
+
+// Find the solution
+function search() {
+  if (found)
+    return;
+  // No columns left mean we have a solution
+  if (header.right == header) {
+    outputSolution(solution);
+    found = true;
+    return;
+  }
+
+  // Chose most constrained column
+  var column = getMinColumn();
+
+  // Cover collisions
+  cover(column);
+
+  for (var rowNode = column.down; rowNode.rowID != column.rowID; rowNode = rowNode.down) {
+    solution.push(rowNode.rowID);
+    for (var rightNode = rowNode.right; rightNode.colID != rowNode.colID; rightNode = rightNode.right)
+      cover(rightNode);
+    search();
+    solution.pop();
+
+    column = rowNode.column;
+    for(var leftNode = rowNode.left; leftNode != rowNode; leftNode = leftNode.left)
+      uncover(leftNode);
+  }
+  uncover(column)
+}
--- js/sudoku.js
+++ js/sudoku.js
@@ -1,21 +1,9 @@
-var grid = makeGrid();
-fillGrid(grid);
+var grid = make2d(9);
 displayGrid(grid);
-
-function makeGrid() {
-  temp = new Array(9);
-  for(var i = 0; i < 9; i++)
-    temp[i] = new Array(9);
-  return temp;
-}
-
-function fillGrid(arrayToFill) {
-  var puzzle = new Array(81);
-  for(var i = 0; i < arrayToFill.length; i++) {
-    for(var j = 0; j < arrayToFill[i].length; j++)
-      arrayToFill[i][j] = puzzle[9*i+j];
-  }
-}
+var bitMatrix = initBitMatrix();
+var nodeMatrix = initNodeMatrix();
+makeDonut(bitMatrix, nodeMatrix);
+bitMatrix = null;
 
 function displayGrid(arrayToDisplay) {
   var table = "<table><tbody><tr>";
@@ -51,20 +39,11 @@ function displayGrid(arrayToDisplay) {
   document.write(table);
 }
 
-function isNumberKey(evt){
+function isNumberKey(evt) {
   var charCode = (evt.which) ? evt.which : event.keyCode
   if (charCode < 49 || charCode > 57)
     return false;
   return true;
-}
-
-function sort(arr) {
-  return arr.concat().sort();
-}
-
-function solve(arrayToSolve) {
-  updateGrid(arrayToSolve);
-  checkGrid(arrayToSolve)
 }
 
 function updateGrid(arrayToUpdate) {
@@ -76,65 +55,17 @@ function updateGrid(arrayToUpdate) {
   }
 }
 
-function checkGrid(arrayToCheck) {
-  if(checkRows(arrayToCheck) && checkCollumns(arrayToCheck) && checkSquares(arrayToCheck)) {
-    console.log("correct")
-    $(".btn").addClass('colorShiftGreen');
-    setTimeout(function(){
-      $(".btn").removeClass('colorShiftGreen');
-      $(".btn").addClass('colorShiftBlack');
-    }, 1500);
-    $(".btn").removeClass('colorShiftBlack');
-  }
-  else {
-    $(".btn").addClass('colorShiftRed');
-    setTimeout(function(){
-      $(".btn").removeClass('colorShiftRed');
-      $(".btn").addClass('colorShiftBlack');
-    }, 1500);
-    $(".btn").removeClass('colorShiftBlack');
-  }
-}
-
-function checkRows(arrayToCheck) {
+function updateTable(updateArray) {
   for(var i = 0; i < 9; i++) {
-    var temp = sort(arrayToCheck[i]);
     for(var j = 0; j < 9; j++) {
-      if(temp[j] != j + 1)
-        return false;
+      tempid = "t" + i + j;
+      document.getElementById(tempid).value = updateArray[i][j];
     }
   }
-  return true;
 }
 
-function checkCollumns(arrayToCheck) {
-  for(var j = 0; j < 9; j++) {
-    var temp = new Array(9);
-    for(var i = 0; i < 9; i++)
-      temp.push(arrayToCheck[i][j]);
-    var sorted = sort(temp);
-    for(var k = 0; k < 9; k++) {
-      if(sorted[k] != k + 1)
-        return false;
-    }
-  }
-  return true;
-}
-
-function checkSquares(arrayToCheck) {
-  for(var i = 0; i < 9; i += 3) {
-    for(var j = 0; j < 9; j += 3) {
-      var temp = new Array(9);
-      for(var m = 0; m < 3; m++) {
-        for(var n = 0; n < 3; n++)
-          temp.push(arrayToCheck[i+m][j+n])
-      }
-      var sorted = sort(temp);
-      for(var k = 0; k < 9; k++) {
-        if(sorted[k] != k+1)
-          return false;
-      }
-    }
-  }
-  return true;
+function solve(arrayToSolve) {
+  updateGrid(arrayToSolve);
+  chooseGiven(arrayToSolve);
+  search();
 }
