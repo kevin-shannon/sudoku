e3cbd7c35a9f05e7ddee03bd81ede51fba101abb
unknown <kevers429@gmail.com>
2020-07-28 10:25:30 -0700

    load bitmatrix as json

--- js/DLX.js
+++ js/DLX.js
@@ -6,34 +6,13 @@ var cols = 324;
 var solution = [];
 var found = false;
 
-var getRight = (i) => (i + 1) % cols;
-var getLeft = (i) => (i - 1 < 0) ? cols - 1 : i - 1;
-var getUp = (i) => (i - 1 < 0) ? rows : i - 1;
-var getDown = (i) => (i + 1) % (rows + 1);
-
-function initBitMatrix() {
-  var bitMatrix = make2d(rows+1);
-  // Building Block matricies
-  var I = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-  var l = [[1], [1], [1]];
-
-  // Building up the constraints using kronecker products
-  rc = kron(kron(kron(I, I), kron(I, I)), kron(l, l));
-  rn = kron(kron(kron(I, I), kron(l, l)), kron(I, I));
-  cn = kron(kron(l, l), kron(kron(I, I), kron(I, I)));
-  bn = kron(kron(kron(kron(I, l), I), l), kron(I, I));
-
-  // Add row of 0's at top :/
-  bitMatrix[0] = new Array(cols).fill(0);
-
-  // Combine the four arrays side by side
-  for (var i = 0; i < rows; i++)
-    bitMatrix[i+1] = rc[i].concat(rn[i].concat(cn[i].concat(bn[i])));
-  return bitMatrix
-}
+var getRight = i => (i + 1) % cols;
+var getLeft = i => (i - 1 < 0 ? cols - 1 : i - 1);
+var getUp = i => (i - 1 < 0 ? rows : i - 1);
+var getDown = i => (i + 1) % (rows + 1);
 
 function initNodeMatrix() {
-  var nodeMatrix = make2d(rows+1);
+  var nodeMatrix = make2d(rows + 1);
   for (var i = 0; i <= rows; i++) {
     for (var j = 0; j < cols; j++) {
       nodeMatrix[i][j] = new Node();
@@ -48,14 +27,12 @@ function makeDonut(bitMatrix, nodeMatrix) {
   // Extra row for headers
   for (var i = 0; i <= rows; i++) {
     for (var j = 0; j < cols; j++) {
-      if(!i)
-        nodeMatrix[0][j].nodeCnt = 0;
+      if (!i) nodeMatrix[0][j].nodeCnt = 0;
 
       // 1 corresponds with node
       if (bitMatrix[i][j] || !i) {
         // Increment nodeCnt if not a header node
-        if (i)
-          nodeMatrix[0][j].nodeCnt++;
+        if (i) nodeMatrix[0][j].nodeCnt++;
 
         nodeMatrix[i][j].column = nodeMatrix[0][j];
         nodeMatrix[i][j].rowID = i;
@@ -63,23 +40,34 @@ function makeDonut(bitMatrix, nodeMatrix) {
 
         // Link nodes
 
-        a = i; b = j;
-        do {b = getLeft(b);} while(!bitMatrix[a][b] && i && b != j);
+        a = i;
+        b = j;
+        do {
+          b = getLeft(b);
+        } while (!bitMatrix[a][b] && i && b != j);
         nodeMatrix[i][j].left = nodeMatrix[i][b];
 
-        a = i; b = j;
-        do {b = getRight(b);} while(!bitMatrix[a][b] && i && b != j);
+        a = i;
+        b = j;
+        do {
+          b = getRight(b);
+        } while (!bitMatrix[a][b] && i && b != j);
         nodeMatrix[i][j].right = nodeMatrix[i][b];
 
-        a = i; b = j;
-        do {a = getUp(a);} while(!bitMatrix[a][b] && a && a != i);
+        a = i;
+        b = j;
+        do {
+          a = getUp(a);
+        } while (!bitMatrix[a][b] && a && a != i);
         nodeMatrix[i][j].up = nodeMatrix[a][j];
 
-        a = i; b = j;
-        do {a = getDown(a);} while(!bitMatrix[a][b] && a && a != i);
+        a = i;
+        b = j;
+        do {
+          a = getDown(a);
+        } while (!bitMatrix[a][b] && a && a != i);
         nodeMatrix[i][j].down = nodeMatrix[a][j];
-      }
-      else {
+      } else {
         nodeMatrix[i][j] = null;
       }
     }
@@ -100,8 +88,16 @@ function cover(target) {
   colNode.right.left = colNode.left;
 
   // Move down and remove nodes traversing to the right
-  for (var rowNode = colNode.down; rowNode.rowID != colNode.rowID; rowNode = rowNode.down) {
-    for (var rightNode = rowNode.right; rightNode.colID != rowNode.colID; rightNode = rightNode.right) {
+  for (
+    var rowNode = colNode.down;
+    rowNode.rowID != colNode.rowID;
+    rowNode = rowNode.down
+  ) {
+    for (
+      var rightNode = rowNode.right;
+      rightNode.colID != rowNode.colID;
+      rightNode = rightNode.right
+    ) {
       rightNode.up.down = rightNode.down;
       rightNode.down.up = rightNode.up;
       nodeMatrix[0][rightNode.colID].nodeCnt--;
@@ -113,8 +109,16 @@ function uncover(target) {
   var colNode = target.column;
 
   // Move down and remove nodes traversing to the right
-  for (var rowNode = colNode.up; rowNode.rowID != colNode.rowID; rowNode = rowNode.up) {
-    for (var leftNode = rowNode.left; leftNode.colID != rowNode.colID; leftNode = leftNode.left) {
+  for (
+    var rowNode = colNode.up;
+    rowNode.rowID != colNode.rowID;
+    rowNode = rowNode.up
+  ) {
+    for (
+      var leftNode = rowNode.left;
+      leftNode.colID != rowNode.colID;
+      leftNode = leftNode.left
+    ) {
       leftNode.up.down = leftNode;
       leftNode.down.up = leftNode;
       nodeMatrix[0][leftNode.colID].nodeCnt++;
@@ -131,17 +135,19 @@ function getMinColumn() {
   var minCol = h.right;
   h = h.right.right;
   do {
-    if (h.nodeCnt < minCol.nodeCnt)
-      minCol = h;
+    if (h.nodeCnt < minCol.nodeCnt) minCol = h;
     h = h.right;
-  } while(h != header);
+  } while (h != header);
   return minCol;
 }
 
 // Returns Node in row, if any
 function getNodeFromRow(row) {
   for (var i = 0; i < cols; i++) {
-    if (nodeMatrix[row][i] && nodeMatrix[row][i].column.left.right == nodeMatrix[row][i].column)
+    if (
+      nodeMatrix[row][i] &&
+      nodeMatrix[row][i].column.left.right == nodeMatrix[row][i].column
+    )
       return nodeMatrix[row][i];
   }
 }
@@ -150,7 +156,11 @@ function getNodeFromRow(row) {
 function choose(row) {
   var rowNode = getNodeFromRow(row);
   cover(rowNode);
-  for (var rightNode = rowNode.right; rightNode.colID != rowNode.colID; rightNode = rightNode.right)
+  for (
+    var rightNode = rowNode.right;
+    rightNode.colID != rowNode.colID;
+    rightNode = rightNode.right
+  )
     cover(rightNode);
 }
 
@@ -159,8 +169,8 @@ function chooseGiven(grid) {
   for (var i = 0; i < 9; i++) {
     for (var j = 0; j < 9; j++) {
       if (grid[i][j]) {
-        choose(81*i + 9*j + parseInt(grid[i][j]));
-        solution.push(81*i + 9*j + parseInt(grid[i][j]));
+        choose(81 * i + 9 * j + parseInt(grid[i][j]));
+        solution.push(81 * i + 9 * j + parseInt(grid[i][j]));
       }
     }
   }
@@ -171,7 +181,7 @@ function convertSolution(solution) {
   var grid = make2d(9);
   for (var i = 0; i < 9; i++) {
     for (var j = 0; j < 9; j++) {
-      grid[i][j] = solution[9*i + j] - (81*i) - (9*j);
+      grid[i][j] = solution[9 * i + j] - 81 * i - 9 * j;
     }
   }
   return grid;
@@ -186,10 +196,15 @@ function outputSolution(solution) {
   solution = [];
 }
 
+var uniqueSolutions = 0;
+
 // Find the solution
 function search() {
+  if (uniqueSolutions > 1) return;
   // No columns left mean we have a solution
   if (header.right == header) {
+    uniqueSolutions++;
+    console.log("unique sol");
     outputSolution(solution);
     return;
   }
@@ -200,16 +215,28 @@ function search() {
   // Cover collisions
   cover(column);
 
-  for (var rowNode = column.down; rowNode.rowID != column.rowID; rowNode = rowNode.down) {
+  for (
+    var rowNode = column.down;
+    rowNode.rowID != column.rowID;
+    rowNode = rowNode.down
+  ) {
     solution.push(rowNode.rowID);
-    for (var rightNode = rowNode.right; rightNode.colID != rowNode.colID; rightNode = rightNode.right)
+    for (
+      var rightNode = rowNode.right;
+      rightNode.colID != rowNode.colID;
+      rightNode = rightNode.right
+    )
       cover(rightNode);
     search();
     solution.pop();
 
     column = rowNode.column;
-    for(var leftNode = rowNode.left; leftNode != rowNode; leftNode = leftNode.left)
+    for (
+      var leftNode = rowNode.left;
+      leftNode != rowNode;
+      leftNode = leftNode.left
+    )
       uncover(leftNode);
   }
-  uncover(column)
+  uncover(column);
 }
--- js/sudoku.js
+++ js/sudoku.js
@@ -5,28 +5,28 @@ preprocess();
 
 function displayGrid(arrayToDisplay) {
   var table = "<table><tbody><tr>";
-  for(var i = 0; i < 9; i++) {
-    for(var j = 0; j < 9; j++) {
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
       var ident;
-      if(i % 3 == 0 && j % 3 == 0)
-        ident = "tl";
-      else if(i == 8 && j % 3 == 0)
-        ident = "bl";
-      else if(i % 3 == 0 && j == 8)
-        ident = "tr";
-      else if(i == 8 && j == 8)
-        ident = "br";
-      else if(i % 3 == 0)
-        ident = "tt";
-      else if((i % 3 == 1 || i % 3 == 2) && j % 3 == 0)
-        ident = "ll";
-      else if(i == 8)
-        ident = "bb";
-      else if(j == 8)
-        ident = "rr";
-      else
-        ident = "ne"
-      table += "<td class = '" + ident + "'><input autocomplete='new-password', tabindex ='1', maxlength = 1, size='1', id = 't" + i + j + "', onkeypress='document.getElementById(\"t" + i + j + "\").value = \"\"; return isNumberKey(event)'></td>";
+      if (i % 3 == 0 && j % 3 == 0) ident = "tl";
+      else if (i == 8 && j % 3 == 0) ident = "bl";
+      else if (i % 3 == 0 && j == 8) ident = "tr";
+      else if (i == 8 && j == 8) ident = "br";
+      else if (i % 3 == 0) ident = "tt";
+      else if ((i % 3 == 1 || i % 3 == 2) && j % 3 == 0) ident = "ll";
+      else if (i == 8) ident = "bb";
+      else if (j == 8) ident = "rr";
+      else ident = "ne";
+      table +=
+        "<td class = '" +
+        ident +
+        "'><input autocomplete='new-password', tabindex ='1', maxlength = 1, size='1', id = 't" +
+        i +
+        j +
+        "', onkeypress='document.getElementById(\"t" +
+        i +
+        j +
+        '").value = ""; return isNumberKey(event)\'></td>';
     }
     table += "</tr><tr>";
   }
@@ -35,23 +35,21 @@ function displayGrid(arrayToDisplay) {
 }
 
 function clearTable() {
-  for(var i = 0; i < 9; i++) {
-    for(var j = 0; j < 9; j++)
-      grid[i][j] = "";
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) grid[i][j] = "";
   }
   updateTable(grid);
 }
 
 function isNumberKey(evt) {
-  var charCode = (evt.which) ? evt.which : event.keyCode
-  if (charCode < 49 || charCode > 57)
-    return false;
+  var charCode = evt.which ? evt.which : event.keyCode;
+  if (charCode < 49 || charCode > 57) return false;
   return true;
 }
 
 function updateGrid(arrayToUpdate) {
-  for(var i = 0; i < 9; i++) {
-    for(var j = 0; j < 9; j++) {
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
       tempid = "t" + i + j;
       arrayToUpdate[i][j] = document.getElementById(tempid).value;
     }
@@ -59,8 +57,8 @@ function updateGrid(arrayToUpdate) {
 }
 
 function updateTable(updateArray) {
-  for(var i = 0; i < 9; i++) {
-    for(var j = 0; j < 9; j++) {
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
       tempid = "t" + i + j;
       document.getElementById(tempid).value = updateArray[i][j];
     }
@@ -68,10 +66,16 @@ function updateTable(updateArray) {
 }
 
 function preprocess() {
-  bitMatrix = initBitMatrix();
-  nodeMatrix = initNodeMatrix();
-  makeDonut(bitMatrix, nodeMatrix);
-  bitMatrix = null;
+  $.ajax({
+    dataType: "json",
+    url: "js/bitmatrix.json",
+    async: false,
+    success: function(json) {
+      bitMatrix = json;
+      nodeMatrix = initNodeMatrix();
+      makeDonut(bitMatrix, nodeMatrix);
+    }
+  });
 }
 
 function solve(arrayToSolve) {
@@ -80,50 +84,54 @@ function solve(arrayToSolve) {
   var t0 = performance.now();
   search();
   var t1 = performance.now();
-  console.log("Solve Time: " + (t1 - t0) + " milliseconds.")
+  console.log("Solve Time: " + (t1 - t0) + " milliseconds.");
   preprocess();
 }
 
-var currCell = $('td').first();
+var currCell = $("td").first();
 
 // User clicks on a cell
-$('td').click(function() {
-    currCell = $(this);
+$("td").click(function() {
+  currCell = $(this);
 });
 
 // User navigates table using keyboard
-$('table').keydown(function (e) {
-    var c = "";
-    if (e.which == 39) {
-        // Right Arrow
-        c = currCell.next();
-    } else if (e.which == 37) {
-        // Left Arrow
-        c = currCell.prev();
-    } else if (e.which == 38) {
-        // Up Arrow
-        c = currCell.closest('tr').prev().find('td:eq(' +
-          currCell.index() + ')');
-    } else if (e.which == 40) {
-        // Down Arrow
-        c = currCell.closest('tr').next().find('td:eq(' +
-          currCell.index() + ')');
-    } else if (e.which == 13 || e.which == 32) {
-        // Enter or Spacebar - edit cell
-        e.preventDefault();
-    } else if (e.which == 9 && !e.shiftKey) {
-        // Tab
-        e.preventDefault();
-        c = currCell.next();
-    } else if (e.which == 9 && e.shiftKey) {
-        // Shift + Tab
-        e.preventDefault();
-        c = currCell.prev();
-    }
+$("table").keydown(function(e) {
+  var c = "";
+  if (e.which == 39) {
+    // Right Arrow
+    c = currCell.next();
+  } else if (e.which == 37) {
+    // Left Arrow
+    c = currCell.prev();
+  } else if (e.which == 38) {
+    // Up Arrow
+    c = currCell
+      .closest("tr")
+      .prev()
+      .find("td:eq(" + currCell.index() + ")");
+  } else if (e.which == 40) {
+    // Down Arrow
+    c = currCell
+      .closest("tr")
+      .next()
+      .find("td:eq(" + currCell.index() + ")");
+  } else if (e.which == 13 || e.which == 32) {
+    // Enter or Spacebar - edit cell
+    e.preventDefault();
+  } else if (e.which == 9 && !e.shiftKey) {
+    // Tab
+    e.preventDefault();
+    c = currCell.next();
+  } else if (e.which == 9 && e.shiftKey) {
+    // Shift + Tab
+    e.preventDefault();
+    c = currCell.prev();
+  }
 
-    // If we didn't hit a boundary, update the current cell
-    if (c.length > 0) {
-        currCell = c;
-        currCell.children(":first").focus();
-    }
+  // If we didn't hit a boundary, update the current cell
+  if (c.length > 0) {
+    currCell = c;
+    currCell.children(":first").focus();
+  }
 });
