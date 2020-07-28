36f01d4c9380eb7847388364cc99afb91f0f8e69
unknown <kevers429@gmail.com>
2020-07-28 05:27:02 -0700

    added arrow navigation

--- js/sudoku.js
+++ js/sudoku.js
@@ -83,3 +83,47 @@ function solve(arrayToSolve) {
   console.log("Solve Time: " + (t1 - t0) + " milliseconds.")
   preprocess();
 }
+
+var currCell = $('td').first();
+
+// User clicks on a cell
+$('td').click(function() {
+    currCell = $(this);
+});
+
+// User navigates table using keyboard
+$('table').keydown(function (e) {
+    var c = "";
+    if (e.which == 39) {
+        // Right Arrow
+        c = currCell.next();
+    } else if (e.which == 37) {
+        // Left Arrow
+        c = currCell.prev();
+    } else if (e.which == 38) {
+        // Up Arrow
+        c = currCell.closest('tr').prev().find('td:eq(' +
+          currCell.index() + ')');
+    } else if (e.which == 40) {
+        // Down Arrow
+        c = currCell.closest('tr').next().find('td:eq(' +
+          currCell.index() + ')');
+    } else if (e.which == 13 || e.which == 32) {
+        // Enter or Spacebar - edit cell
+        e.preventDefault();
+    } else if (e.which == 9 && !e.shiftKey) {
+        // Tab
+        e.preventDefault();
+        c = currCell.next();
+    } else if (e.which == 9 && e.shiftKey) {
+        // Shift + Tab
+        e.preventDefault();
+        c = currCell.prev();
+    }
+
+    // If we didn't hit a boundary, update the current cell
+    if (c.length > 0) {
+        currCell = c;
+        currCell.children(":first").focus();
+    }
+});
