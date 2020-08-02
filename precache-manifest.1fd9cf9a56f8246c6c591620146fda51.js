b689ab92cd1e7523a5359f6941ad91e36731aad2
kevin-shannon <kevin-shannon@users.noreply.github.com>
2020-08-02 19:12:46 +0000

    deploy: adc00fe4b6605e50845f1160459121db1dc42568

---
+++ precache-manifest.1fd9cf9a56f8246c6c591620146fda51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042e76aa7449296e35cd141df51aa923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f4841a499135d6cb798",
+    "url": "/static/css/main.600ff82f.chunk.css"
+  },
+  {
+    "revision": "a87104d09c650664c927",
+    "url": "/static/js/2.9001f09c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9001f09c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4841a499135d6cb798",
+    "url": "/static/js/main.9d5a46db.chunk.js"
+  },
+  {
+    "revision": "c71dd58cd5349d4e6ab8",
+    "url": "/static/js/runtime-main.4dd858cb.js"
+  },
+  {
+    "revision": "946f1626568779d9773d9eb1c20a063c",
+    "url": "/static/media/irohamaru-mikami-Regular.946f1626.ttf"
+  }
+]);
